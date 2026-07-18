a661d3faa0a23e6ab6cd27cba2f2f142d0e18020
Ton@ <alwaysimproveyourself1@gmail.com>
2026-07-18 17:15:46 +0300

    fixed configure port groups

--- terraform/foundation/network.tf
+++ terraform/foundation/network.tf
@@ -1,22 +1,89 @@
 locals {
   port_groups = {
-    "PG-V10-Management"     = 10
-    "PG-V20-Infrastructure" = 20
-    "PG-V30-ControlPlane"   = 30
-    "PG-V40-etcd"           = 40
-    "PG-V50-Workers"        = 50
-    "PG-V60-Edge"           = 60
-    "PG-V70-Monitoring"     = 70
-    "PG-V80-Databases"      = 80
-    "PG-V90-Storage"        = 90
-    "PG-V99-Transit"        = 99
+    management = {
+      name        = "PG-V10-Management"
+      vlan        = 10
+      cidr        = "10.50.10.0/24"
+      description = "Management network"
+    }
+
+    infrastructure = {
+      name = "PG-V20-Infrastructure"
+      vlan = 20
+      cidr = "10.50.20.0/24"
+    }
+
+    control_plane = {
+      name = "PG-V30-ControlPlane"
+      vlan = 30
+      cidr = "10.50.30.0/24"
+    }
+
+    etcd = {
+      name = "PG-V40-etcd"
+      vlan = 40
+      cidr = "10.50.40.0/24"
+    }
+
+    workers = {
+      name = "PG-V50-Workers"
+      vlan = 50
+      cidr = "10.50.50.0/24"
+    }
+
+    edge = {
+      name = "PG-V60-Edge"
+      vlan = 60
+      cidr = "10.50.60.0/24"
+    }
+
+    monitoring = {
+      name = "PG-V70-Monitoring"
+      vlan = 70
+      cidr = "10.50.70.0/24"
+    }
+
+    databases = {
+      name = "PG-V80-Databases"
+      vlan = 80
+      cidr = "10.50.80.0/24"
+    }
+
+    storage = {
+      name = "PG-V90-Storage"
+      vlan = 90
+      cidr = "10.50.90.0/24"
+    }
+
+    transit = {
+      name = "PG-V99-Transit"
+      vlan = 99
+      cidr = "10.50.99.0/24"
+      vrrp = true
+    }
+    # Trunk network for VyOS router interfaces
+    trunk = {
+      name = "PG-VGT-Trunk"
+      vlan = 4095
+      vrrp = true
+    }
   }
 }
 
 resource "vsphere_host_port_group" "pgs" {
-  for_each            = local.port_groups
-  name                = each.key
+  for_each = local.port_groups
+
+  name                = each.value.name
   host_system_id      = data.vsphere_host.esxi_host.id
   virtual_switch_name = var.vsphere_standard_switch_name
-  vlan_id             = each.value
+  vlan_id             = each.value.vlan
+
+  # CRITICAL FOR VRRP: Override default vSphere security settings
+  allow_promiscuous = false # Keep false unless troubleshooting packet captures
+
+  # Required for VyOS to accept the VRRP Virtual MAC
+  allow_mac_changes = try(each.value.vrrp, false)
+
+  # Required for VyOS to transmit traffic using the VRRP Virtual MAC
+  allow_forged_transmits = try(each.value.vrrp, false)
 }
