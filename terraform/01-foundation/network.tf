3ee5abe87ca3aba057c6ca4f5001de84c992477c
Ton@ <alwaysimproveyourself1@gmail.com>
2026-07-23 11:42:18 +0300

    fxied network mode allow_promiscuous=true

--- terraform/01-foundation/network.tf
+++ terraform/01-foundation/network.tf
@@ -11,48 +11,56 @@ locals {
       name = "PG-V20-Infrastructure"
       vlan = 20
       cidr = "10.50.20.0/24"
+      vrrp = true # <-- Added
     }
 
     control_plane = {
       name = "PG-V30-ControlPlane"
       vlan = 30
       cidr = "10.50.30.0/24"
+      vrrp = true # <-- Added
     }
 
     etcd = {
       name = "PG-V40-etcd"
       vlan = 40
       cidr = "10.50.40.0/24"
+      vrrp = true # <-- Added
     }
 
     workers = {
       name = "PG-V50-Workers"
       vlan = 50
       cidr = "10.50.50.0/24"
+      vrrp = true # <-- Added
     }
 
     edge = {
       name = "PG-V60-Edge"
       vlan = 60
       cidr = "10.50.60.0/24"
+      vrrp = true # <-- Added
     }
 
     monitoring = {
       name = "PG-V70-Monitoring"
       vlan = 70
       cidr = "10.50.70.0/24"
+      vrrp = true # <-- Added
     }
 
     databases = {
       name = "PG-V80-Databases"
       vlan = 80
       cidr = "10.50.80.0/24"
+      vrrp = true # <-- Added
     }
 
     storage = {
       name = "PG-V90-Storage"
       vlan = 90
       cidr = "10.50.90.0/24"
+      vrrp = true # <-- Added
     }
 
     transit = {
@@ -61,6 +69,7 @@ locals {
       cidr = "10.50.99.0/24"
       vrrp = true
     }
+
     # Trunk network for VyOS router interfaces
     trunk = {
       name = "PG-VGT-Trunk"
@@ -69,6 +78,7 @@ locals {
     }
   }
 }
+
 
 resource "vsphere_host_port_group" "pgs" {
   for_each = local.port_groups
