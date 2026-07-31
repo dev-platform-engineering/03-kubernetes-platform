d984c9de3ef919d6289782096ea415c1a5ba6bcf
Ton@ <alwaysimproveyourself1@gmail.com>
2026-07-31 16:28:32 +0300

    fixed error during create vm in compute

--- terraform/03-compute/data.tf
+++ terraform/03-compute/data.tf
@@ -14,29 +14,3 @@ data "vsphere_network" "networks" {
   name          = each.value
   datacenter_id = data.terraform_remote_state.foundation.outputs.datacenter_id
 }
-
-# data "vsphere_datacenter" "dc" {
-#   name = var.datacenter
-# }
-
-# data "vsphere_host" "esxi_host" {
-#   name          = var.esxi_host
-#   datacenter_id = data.vsphere_datacenter.dc.id
-# }
-
-# data "vsphere_datastore" "this" {
-#   for_each = var.datastores
-
-#   name          = each.value
-#   datacenter_id = data.vsphere_datacenter.dc.id
-# }
-
-# data "vsphere_virtual_machine" "templates" {
-#   for_each = {
-#     for vm in local.repository_vms :
-#     vm.template_name => vm
-#   }
-
-#   name          = each.key
-#   datacenter_id = module.foundation.datacenter_id
-# }
